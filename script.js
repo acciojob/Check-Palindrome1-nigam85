1f75e8f0da05d6b5813f2ac2596956f2dec3b6ad
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-27 14:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+	var reversed = str.split("").reversed().join("")
+	if(reversed===str){
+		return true;
+	}else return false
 
 }
+// console.log(palindrome())
 module.exports = palindrome
